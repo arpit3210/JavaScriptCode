1d03ddbd997161321ff9df0dd6bce3453490af70
1234 <arpitsinghthakur321@gmail.com>
2023-08-25 16:00:33 +0530

    primitive data type and non primitive data type

---
+++ 3primitive.js
@@ -0,0 +1,23 @@
+//www.youtube.com/watch?v=qpU3WIqRz9I&list=PLu0W_9lII9ahR1blWXxgSlL4y9iQBnLpR&index=4
+
+// Primitive in js
+let a= null;
+let b= 37564;
+let c= false;
+let d= BigInt("234") + BigInt("87");
+let e= "Kuchbhyi"  ;
+let f= Symbol("I am symbol");
+let g= undefined;
+
+console.log(a,b,c,d,e,f,g);
+console.log(typeof c);
+
+
+// Object in js   // Non primitive Data Type
+ const item = {
+    Name : "Arpit",
+    Class : false,
+    Rollno : 3784
+ }
+
+ console.log(item.Name);
