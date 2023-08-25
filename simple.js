0857470b67afb9d49df4507d971507b6a8c37bf0
1234 <arpitsinghthakur321@gmail.com>
2023-08-25 15:19:08 +0530

    use of varibales

---
+++ simple.js
@@ -0,0 +1,11 @@
+let num = 34;
+
+console.log(num);
+
+num = "arpit";
+
+console.log(num);
+
+
+// https://drive.google.com/drive/folders/1RcyvvOAxJtNc_mPLt9g-kTHIMUZ3uD8z
+// PDF dOCUMENT FOR LEARNING
---
+++ variable.js
@@ -0,0 +1,17 @@
+// var, let , const
+
+var a= "arpit";
+console.log(a);
+var a= "shankey";
+console.log(a);
+ a= "somethigs";
+ console.log(a);
+{
+    let a= "under block"
+    console.log(a);
+}
+
+
+const b= 38764;
+// const c; this is not allowed 
+// you have to declare the value as well
