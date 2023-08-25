7f87b0685d7badf4f0eb23fcf344110dff5bdf47
1234 <arpitsinghthakur321@gmail.com>
2023-08-25 17:23:20 +0530

    Expression and Logical operations assignment operators

---
+++ 6expression.js
@@ -0,0 +1,64 @@
+
+
+console.log("Operators in JS")
+
+let a= 10;
+let b= 4;
+
+console.log("a+b=" , a+b);
+console.log("a-b=" , a-b);
+console.log("a*b=" , a*b);
+console.log("a/b=" , a/b);
+console.log("a**b=" , a**b);
+console.log("a%b=" , a%b);
+console.log("++a=" , ++a);
+console.log("a++=" , a++);
+console.log("--a=" , --a);
+console.log("a--=" , a--);
+console.log("a=" , a);
+console.log("a--" , a--);
+
+
+
+
+// Assigment Operaotrs
+
+// = x=y 
+// +=
+// -+
+// *=
+// /=
+// %=
+// **=
+
+
+console.log(a);   // output -- 9
+a += b;
+console.log(a);
+// output 
+
+
+
+
+// Comparison Operaotrs 
+
+let comp1 = 6;
+let comp2 = "6";
+
+console.log("comp1 == comp2", comp1 == comp2);
+console.log("comp1 != comp2", comp1 != comp2);
+console.log("comp1 === comp2", comp1 === comp2);  //  Check Typeof value comparison  as well with comparision
+console.log("comp1 !== comp2", comp1 !== comp2);
+console.log("comp1 > comp2", comp1 > comp2);
+
+
+
+// Logical Operators
+
+let x= 6;
+let y= 6;
+
+console.log(x>y && x==5);
+console.log(x>y || x==y);
+console.log(!true);
+console.log(!false);
---
+++ Chapter1.js
@@ -0,0 +1,34 @@
+//  chapter 1- q1
+
+let a= "Arpit";
+let b= 6
+console.log(a+b);
+
+
+//  Quetion 2 
+console.log(typeof (a+b));
+
+
+// const a; 
+// this is not possible if you are declaring a const variable you have to define the value as well
+const a1= {
+    Name : "Arpit",
+    Class : false,
+    RoolNo : 3645
+}
+// a1= {} ;  This is not pssible
+a1["Quantity"]= "Diffrent";
+// adding a different key value pair in object is possible but create another object in not posssible
+console.log(a1);
+
+
+
+const wordmeaning = ["Blockchain", "consensus", "Mechanism", "Polakadot", "Ethereum"]
+console.log(wordmeaning);
+
+
+
+// output
+// Arpit6
+// string
+// [ 'Blockchain', 'consensus', 'Mechanism', 'Polakadot', 'Ethereum' ]
